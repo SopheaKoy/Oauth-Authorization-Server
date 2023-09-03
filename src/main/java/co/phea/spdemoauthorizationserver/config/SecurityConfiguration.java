772550b353081fb5578a1e271e6dc52949359c0d
SopheaKoy <koysophea09@gmail.com>
2023-09-03 23:54:13 +0700

    Update AccessToken expiration

--- src/main/java/co/phea/spdemoauthorizationserver/config/SecurityConfiguration.java
+++ src/main/java/co/phea/spdemoauthorizationserver/config/SecurityConfiguration.java
@@ -18,7 +18,6 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
@@ -29,6 +28,7 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -38,6 +38,7 @@ import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.time.Duration;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -78,24 +79,23 @@ public class SecurityConfiguration {
     @Bean
     public UserDetailsService userDetailsService (){
 
-        var viewer = User.withUsername("dann")
-                .password("password")
+        var viewer = User.withUsername("viewer")
+                .password("pass1234")
                 .authorities("read")
                 .roles("VIEWER")
                 .build();
 
-        var admin = User.withUsername("faa")
-                .password("password")
+        var admin = User.withUsername("admin")
+                .password("pass5678")
                 .authorities("read")
                 .roles("VIEWER","ADMIN")
                 .build();
 
+
         return new InMemoryUserDetailsManager(viewer , admin);
     }
 
-
     // Create Password Encoder
-    // only for dev , don't use same in production
     @Bean
     public PasswordEncoder passwordEncoder(){
 
@@ -108,19 +108,35 @@ public class SecurityConfiguration {
     public RegisteredClientRepository registeredClientRepository() {
 
         var registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+
                 .clientId("public-client-react-app")
+
                 .clientSecret("secret") // it's store manager in secret
+
                 .scope(OidcScopes.OPENID)
+
                 .scope(OidcScopes.PROFILE)
+
+                .tokenSettings(tokenSettings())
+
                 .redirectUri("http://127.0.0.1:8083/login/oauth2/code/public-client-react-app")
+
                 .clientAuthenticationMethod(ClientAuthenticationMethod.NONE)
+
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
+
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+
                 .authorizationGrantTypes(grantType -> {
+
                     grantType.add(AuthorizationGrantType.AUTHORIZATION_CODE);
+
                     grantType.add(AuthorizationGrantType.REFRESH_TOKEN);
+
                     grantType.add(AuthorizationGrantType.CLIENT_CREDENTIALS);
+
                 }).clientSettings(ClientSettings.builder().requireProofKey(true).build())
+
                 .build();
 
         return new InMemoryRegisteredClientRepository(registeredClient);
@@ -190,11 +206,21 @@ public class SecurityConfiguration {
 
                         .collect(Collectors.toSet());
 
-                context.getClaims().claim("authorities",authorities);
+                context.getClaims().claim("authorities", authorities);
 
             }
 
         };
+
+    }
+
+    // Defined Token expiration time
+
+    @Bean
+    public TokenSettings tokenSettings(){
+        return TokenSettings.builder()
+                .accessTokenTimeToLive(Duration.ofHours(2)) // token expiration 2 hours
+                .build();
 
     }
 
